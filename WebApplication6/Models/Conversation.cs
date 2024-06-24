987d5217e266c404a1b375c3d2dc7c50fdd1f0a2
Durgesh Marathe <durgesh.m@samcomtechnobrains.com>
2024-06-24 18:52:34 +0530

    add models

---
+++ WebApplication6/Models/Conversation.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication6.Models
+{
+    public class Conversation
+    {
+        public int Id { get; set; }
+
+        public ICollection<int> Participants { get; set; } = new List<int>();
+
+        public ICollection<Message> Messages { get; set; } = new List<Message>();
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ WebApplication6/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        public string ProfilePic { get; set; }
+
+        public bool Blocked { get; set; }
+
+        // Navigation property for blocked users
+        public ICollection<User> BlockedUsers { get; set; }
+    }
+}
